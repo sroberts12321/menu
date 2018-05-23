2a187ee2e17c808d2d29d5b0866034f341586244
Stephen Roberts <Sroberts12321@gmail.com>
2018-05-23 14:53:21 -0500

    better css styling

--- controller.js
+++ controller.js
@@ -3,12 +3,14 @@ let menuDiv = document.getElementById("menu")
 
 menu.forEach((item)=>{
 	//listing out each attribute
-	let menuItem = `<img class="image" src="${item.imageURL}">
+	let menuItem = `<div class="item">
+	<img class="image" src="${item.imageURL}">
 	<div class="details">
 	<h4 class="titles">${item.title}</h4>
 	<p class="descriptions">${item.description}</p>
 	</div>
 	<h4 class="prices">${item.price}</h4>
+	</div>
 	`
 	menuDiv.innerHTML += menuItem
 })
